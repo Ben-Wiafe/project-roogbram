2d6dc7daec893436ec293a7e14f1396ece798912
AI-Raygan <rkmarfo@st.ug.edu.gh>
2021-11-21 21:23:58 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import Aikins from './components/2body'
 import Features from './components/features'
 import Testimony from './components/testimonial'
 import Footer from './components/footer'
+import Contact from './components/contact'
+ 
 export default function App() {
   return (
-    <div class="px-6 py-3 pt-4">
+    <div class="px-2 py-3 pt-4">
       <Navigation/>
       <FirstBody/>
       <Aikins/>
       <Features/>
       <Testimony/>
+      <Contact/>
       <Footer/>
     </div>
   )
