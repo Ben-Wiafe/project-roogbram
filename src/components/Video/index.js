5ae700516061a0cc3bc5ce4ba486a4c274a0a999
Azay <azay.genius8@gmail.com>
2021-11-28 02:54:21 +0100

    added youtube link

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -5,8 +5,8 @@ export default function Video() {
     return (
       <div className="sm:flex">
         <div className="mb-4 flex-shrink-0 sm:mb-0 sm:mr-4">
-            <a href="https://www.youtube.com/watch?v=5_8HeGnQF-4" target="_blank">
-         <img class="lg:pt-4 pb-10 w-full ml-16 " alt="Azay" src={video}/> 
+            <a href="https://www.youtube.com/watch?v=5_8HeGnQF-4" target="_self">
+         <img className="lg:pt-4 pb-10 w-full ml-16 " alt="Azay" src={video}/> 
         </a>
         </div>
           
