4f12fb2ad9fef990d7cb2a4d236d09d22edd2534
Azay <azay.genius8@gmail.com>
2021-11-28 02:52:37 +0100

    changed class to className

--- src/components/features/index.js
+++ src/components/features/index.js
@@ -4,15 +4,15 @@ export default function Features() {
     return (
            <div>
               <div>
-               <p class="text-blue-800 flex items-center justify-center font-semibold"> Our  <p class="text-yellow-600 ml-1">Features</p> </p>
-                <p class="items-center justify-center flex text-gray-500">This very extraordinary feature can make learning activities more efficient</p>
+               <p className="text-center justify-center items-center font-semibold text-lg flex  text-blue-900 text-3xl py-2"> Our  <p className="text-yellow-600 ml-1">Features</p> </p>
+                <p className="items-center justify-center flex text-lg text-gray-500">This very extraordinary feature can make learning activities more efficient</p>
            </div>
-           <div class="p-16 flex items-center justify-between">
-               <img class="md:w-full h-850 xs:max-w-lg md:max-w-md sm:w-80 h-80 lg:w-85 h-80 flex justify-between rounded-lg" src={test} alt="test and quizzes"/>
-                <div class="ml-20">
-                <p class="mr-35 font-bold text-blue-800 text-4xl">Assignment and Quizzes</p>
-                <p class="mr-40 font-bold text-blue-800 text-4xl">& Tests</p>
-                <p class="max-w-2xl"> 
+           <div className="p-10 lg:flex items-center justify-around sm:flex md:flex-column ">
+               <img className="md:w-full h-850 pl-12 w-full md:max-w-md lg:w-85 h-80 flex justify-between rounded-lg" src={test} alt="test and quizzes"/>
+                <div className="ml-20 ">
+                <p className="mr-35 font-bold text-blue-800 sm:text-3xl md:text-4xl text-3xl mt-10 lg:text-4xl">Assignment and Quizzes</p>
+                <p className="mr-40 font-bold text-blue-800  text-4xl">& Tests</p>
+                <p className="lg:max-w-2xl sm:max-w-md"> 
                 You define your project’s breakpoints in the theme.screens section of your tailwind.config.js file. The keys are your screen names (used as the prefix for the responsive utility variants Tailwind generates, like md:text-center), 
                 and the values are the min-width where that breakpoint should start.
                 The default breakpoints are inspired by common device resolutions:
