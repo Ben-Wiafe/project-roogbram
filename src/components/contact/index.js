a642a85aa8cf8c4e15e4a6175ba7b65c6755c9e3
Azay <azay.genius8@gmail.com>
2021-11-28 02:52:16 +0100

    Edited texts

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import  React, {useState,useReducer} from 'react';
 import { MailIcon, PhoneIcon } from '@heroicons/react/outline'
 
+
+
+ 
 export default function Contact() {
+  
+
   return (
     <div className="relative bg-white">
       <div className="absolute inset-0">
@@ -10,9 +15,9 @@ export default function Contact() {
       <div className="relative max-w-7xl mx-auto lg:grid lg:grid-cols-5">
         <div className="bg-white py-16 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-24 xl:pr-12">
           <div className="max-w-lg mx-auto">
-            <h2 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">Get in touch</h2>
+            <h2 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl" id="Contact">Get in touch</h2>
             <p className="mt-3 text-lg leading-6 text-gray-500">
-            For all enquiries fill the form below.
+            For all enquiries, kindly  fill the form below.
             </p>
             <dl className="mt-8 text-base text-gray-500">
               <div>
@@ -20,7 +25,7 @@ export default function Contact() {
                 <dd>
                   <p>Chat with a member of our support team from Ghana
                     Hours: 7AM - 10PM GMT, 7 days a week</p>
-                  <p>Springfield, OR 12345</p>
+                  <p>University of Ghana,Legon</p>
                 </dd>
               </div>
               <div className="mt-6">
@@ -43,18 +48,19 @@ export default function Contact() {
         </div>
         <div className="bg-white py-16 px-4 sm:px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
           <div className="max-w-lg mx-auto lg:max-w-none">
-            <form action="#" method="POST" className="grid grid-cols-1 gap-y-6">
+            <form action="#" className="grid grid-cols-1 gap-y-6">
               <div>
-                <label htmlFor="full-name" className="sr-only">
+                <label htmlFor="name" className="sr-only">
                   Full name
                 </label>
                 <input
                   type="text"
-                  name="full-name"
-                  id="full-name"
+                  name="fullname"
+               
                   autoComplete="name"
                   className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-yellow-500 focus:border-yellow-500 border-gray-300 rounded-md"
                   placeholder="Full name"
+            
                 />
               </div>
               <div>
@@ -62,12 +68,13 @@ export default function Contact() {
                   Email
                 </label>
                 <input
-                  id="email"
                   name="email"
+                 
                   type="email"
                   autoComplete="email"
                   className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-yellow-500 focus:border-indigo-500 border-gray-300 rounded-md"
                   placeholder="Email"
+             
                 />
               </div>
               <div>
@@ -75,9 +82,9 @@ export default function Contact() {
                   Phone
                 </label>
                 <input
-                  type="text"
+                  type="tel"
                   name="phone"
-                  id="phone"
+   
                   autoComplete="tel"
                   className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-yellow-500 focus:border-yellow-500 border-gray-300 rounded-md"
                   placeholder="Phone"
@@ -88,21 +95,21 @@ export default function Contact() {
                   Message
                 </label>
                 <textarea
-                  id="message"
-                  name="message"
-                  rows={4}
+             
                   className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-yellow-500 focus:border-yellow-500 border border-gray-300 rounded-md"
                   placeholder="Message"
-                  defaultValue={''}
+                 
                 />
               </div>
               <div>
                 <button
+               
                   type="submit"
                   className="inline-flex justify-center py-3 px-6 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-yellow-500 hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
                 >
                   Submit
                 </button>
+                
               </div>
             </form>
           </div>
