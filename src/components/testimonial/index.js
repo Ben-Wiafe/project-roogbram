ecd999ded5ab2fb3a39a3a6aebba46cf92147942
Azay <azay.genius8@gmail.com>
2021-11-28 02:53:59 +0100

    modified users testimonies

--- src/components/testimonial/index.js
+++ src/components/testimonial/index.js
@@ -5,76 +5,77 @@ import { BsStarFill } from "react-icons/bs";
 
 function Testimony() {
   return (
-    <div>
-      <div class="text-center mt-14 -ml-14 sm:flex-column items-center sm:flex-grow lg:justify-evenly">
-        <p class="text-yellow-500 font-semibold text-md"> Testimonial</p>
-        <p class="text-blue-800 font-semibold text-2xl">
+    <div >
+      <div className="text-center mt-14 -ml-14 sm:flex-column items-center  lg:justify-evenly">
+        <p className="text-yellow-500 font-semibold text-md"> Testimonial</p>
+        <p className="text-blue-800 font-semibold text-2xl">
           {" "}
           What our users say about us
         </p>
       </div>
-      <div class="flex justify-center space-x-10 items-center pt-4 mb-4 ">
-        <div class=" rounded-lg mb-4 max-w- w-2/4 h-1/6 p-4 lg:bg-white shadow lg:shadow-md  sm:shadow-md sm: bg-green-600 md:bg-yellow-500 text-md font-bold sm:max-2-sm lg:max-w-lg">
+      <div className="lg:flex md:flex-column md:justify-between lg:justify-evenly sm:space-x-10  items-center pt-4 mb-4 ">
+        <div className="sm:ml-16 md:ml-16 rounded-lg mb-4  w-2/4 h-1/6 p-4 lg:bg-white shadow lg:shadow-md  sm:shadow-md text-md font-bold sm:max-2-sm lg:max-w-lg">
           {" "}
-          My House Sold Out Fast!
-          <p class="font-normal text-sm">
-            According to a survey by Runzheimer International, the average cost
-            of a fast-food meal (quarter-pound cheeseburger, large fries, medium
-            soft drink, excluding taxes) in Seattle is $4.82. Suppose this
-            figure was based on a sample of 27 different establishments and the
-            standard deviation was $0.37
+          <p className="sm:max-w-lg">
+          This app is very functional as a learning app
           </p>
+          
+          <p className="font-normal text-sm pb-5 mt-1 sm:max-w-xl">
+          This is school at your own time schedule, it's an amazing app, like never before... 
+          I love this app so much. The teaching is very understandable, straight to the point, no adds to distract and lessons of your choice. 
+          It covers all aspects of schooling right from kindergarten to the highest level.
+           Basic explanations to get the concept into your head. I love this app.          </p>
 
-          <div class="flex space-x-3 items-center pt-2">
+          <div className="flex space-x-3 items-center pt-2">
           <img
-            class="w-6 rounded-xl h-6"
+            className="w-6 rounded-xl h-6"
             src={Azay}
             alt="A boy smiling"
           />
           <div></div>
-          <div class="lg:flex-column sm:flex-row max-w-xl md:flex-column max-w-lg">
-            <p class="font-bold text-xs"> Bernard Wiafe</p>
-            <p class="lg:font-normal lg:text-xs sm:text-lg sm:ml-0  sm:mr-5">
+          <div className="lg:flex-column sm:flex-row max-w-xl md:flex-column max-w-lg">
+            <p className="font-bold lg:text-xs md:text-lg sm:text-md"> Bernard Wiafe</p>
+            <p className="lg:font-normal lg:text-xs sm:text-lg sm:ml-0  sm:mr-5">
               Student @ UG
             </p>
           </div>
-          <div class="lg:pl-64">
+          <div className="lg:pl-64">
             <BsStarFill style={{size:15, color:'#F59E0B'}}  />
           </div>
           <div >
-            <p class="ml-0 font-bold text-sm sm:mr-2">4.3</p>
+            <p className="ml-0 font-bold text-sm sm:mr-2">4.3</p>
           </div>
         </div>
         </div>
 
-        <div class=" rounded-lg mb-4 w-2/4 h-1/6 p-4  lg:bg-white shadow lg:shadow-md sm:shadow-md sm: bg-green-600 md:bg-yellow-500 text-md font-bold sm:max-2-sm lg:max-w-lg">
+        <div className="sm:ml-16 md:ml-20 ml-10 rounded-lg mb-4 w-2/4 h-1/6 p-4  lg:bg-white shadow lg:shadow-md sm:shadow-md sm:  text-md font-bold sm:max-2-sm lg:max-w-lg">
           {" "}
-          Best Standard SAT App so far!!
-          <p class="font-normal text-sm">
-            According to a survey by Runzheimer International, the average cost
-            of a fast-food meal (quarter-pound cheeseburger, large fries, medium
-            soft drink, excluding taxes) in Seattle is $4.82. Suppose this
-            figure was based on a sample of 27 different establishments and the
-            standard deviation was $0.37
+          <p className="max-w-xs">
+          I’m doing incredibly well and had 1580 out 1600 on my last SAT mock!
+          </p>
+           <p className="font-normal text-sm sm:max-w-lg">
+          I chanced on this website while surfing the internet and this website meets my SAT needs and requirements from all perspectives. 
+          I take personal SAT mocks under examination conditions and I aced 1580 out of 1600 on my last SAT mock all thanks to this website. 
+          I strongly recommend to all. What are you waiting for? Let’s win together fellas!
           </p>
 
-          <div class="flex space-x-3 items-center pt-2">
+          <div className="flex space-x-3 items-center pt-2">
           <img
-            class="w-6 rounded-xl h-6"
+            className="w-6 rounded-xl h-6"
             src={She}
             alt="A boy smiling"
           />
           <div></div>
-          <div class="lg:flex-column sm:flex-row max-w-xl md:flex-column max-w-lg">
-            <p class="font-bold text-xs"> Richard Amuzu</p>
-            <p class="lg:font-normal lg:text-xs">Student @ KNUST
+          <div className="lg:flex-column sm:flex-row max-w-xl md:flex-column max-w-lg">
+            <p className="font-bold lg:text-xs md:text-md"> Richard Amuzu</p>
+            <p className="lg:font-normal lg:text-xs  md:text-md">Student @ KNUST
             </p>
           </div>
-          <div class="lg:pl-64">
+          <div className="lg:pl-64">
             <BsStarFill style={{size:15, color:'#F59E0B'}}  />
           </div>
           <div >
-            <p class="ml-0 font-bold text-sm sm:mr-2">4.5</p>
+            <p className="ml-0 font-bold text-sm sm:mr-2">4.5</p>
           </div>
         </div>
         </div>
