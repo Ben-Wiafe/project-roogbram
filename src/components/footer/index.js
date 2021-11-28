3d71e76e982cb121a4929d4566ede8e5234cf929
Azay <azay.genius8@gmail.com>
2021-11-28 02:53:03 +0100

    initial commit

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -99,7 +99,7 @@ const navigation = {
         </h2>
         <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
           <div className="xl:grid xl:grid-cols-3 xl:gap-8">
-            <div className="space-y-2 xl:col-span-1">
+            <div className="space-y-2 xl:col-span-1" id="footer">
               <img
                 className="h-10"
                 src={Logo}
@@ -120,7 +120,7 @@ const navigation = {
               <div className="md:grid md:grid-cols-2 md:gap-8">
                 <div>
                   <h3 className="text-sm font-semibold text-gray-900 tracking-wider uppercase">Solutions</h3>
-                  <ul role="list" className="mt-4 space-y-4">
+                  <ul  className="mt-4 space-y-4">
                     {navigation.solutions.map((item) => (
                       <li key={item.name}>
                         <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
@@ -132,7 +132,7 @@ const navigation = {
                 </div>
                 <div className="mt-12 md:mt-0">
                   <h3 className="text-sm font-semibold text-gray-900 tracking-wider uppercase">Support</h3>
-                  <ul role="list" className="mt-4 space-y-4">
+                  <ul className="mt-4 space-y-4">
                     {navigation.support.map((item) => (
                       <li key={item.name}>
                         <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
@@ -146,7 +146,7 @@ const navigation = {
               <div className="md:grid md:grid-cols-2 md:gap-8">
                 <div>
                   <h3 className="text-sm font-semibold text-gray-900 tracking-wider uppercase">Company</h3>
-                  <ul role="list" className="mt-4 space-y-4">
+                  <ul  className="mt-4 space-y-4">
                     {navigation.company.map((item) => (
                       <li key={item.name}>
                         <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
@@ -158,7 +158,7 @@ const navigation = {
                 </div>
                 <div className="mt-12 md:mt-0">
                   <h3 className="text-sm font-semibold text-gray-900 tracking-wider uppercase">Legal</h3>
-                  <ul role="list" className="mt-4 space-y-4">
+                  <ul  className="mt-4 space-y-4">
                     {navigation.legal.map((item) => (
                       <li key={item.name}>
                         <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
